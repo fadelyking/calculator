ff01f073866a44fe47ab084339f9e23fa57dd880
fadelyking <fadely195@gmail.com>
2023-06-06 18:00:47 +0300

    Created a function that populates the first display when you click the number buttons Value is stored into arrays

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-function add(input) {
-	const plus = document.querySelector(".plus");
-	plus.addEventListener("click", () => {
-		return console.log("+");
-	});
-}
+let firstNum = [];
+let operation = "";
+let secondNum = [];
 
-function subtract(input) {
-	const minus = document.querySelector(".minus");
-	minus.addEventListener("click", () => {
-		return console.log("-");
-	});
-}
+const calculatorButtons = [
+	{ num: 0, loc: document.querySelector(".zero") },
+	{ num: 1, loc: document.querySelector(".one") },
+	{ num: 2, loc: document.querySelector(".two") },
+	{ num: 3, loc: document.querySelector(".three") },
+	{ num: 4, loc: document.querySelector(".four") },
+	{ num: 5, loc: document.querySelector(".five") },
+	{ num: 6, loc: document.querySelector(".six") },
+	{ num: 7, loc: document.querySelector(".seven") },
+	{ num: 8, loc: document.querySelector(".eight") },
+	{ num: 9, loc: document.querySelector(".nine") },
+	{ plus: document.querySelector(".plus") },
+	{ minus: document.querySelector(".minus") },
+	{ multiply: document.querySelector(".multiply") },
+	{ divide: document.querySelector(".divide") },
+];
 
-function multiply(input) {
-	const multiply = document.querySelector(".times");
-
-	multiply.addEventListener("click", () => {
-		return console.log("*");
+const numbers = document.querySelector(".numbers");
+numbers.addEventListener("click", (e) => {
+	const getNumber = calculatorButtons.filter((item) => {
+		if (e.target === item.loc) {
+			firstNum.push(item.num);
+		}
+		const display = document.querySelector(".display-container");
+		display.textContent = `${firstNum.join("")}`;
 	});
-}
+});
 
-function divide(input) {
-	const divide = document.querySelector(".divide");
-	divide.addEventListener("click", () => {
-		return console.log("/");
-	});
-}
+function add(input) {}
+
+function subtract(input) {}
+
+function multiply(input) {}
+
+function divide(input) {}
 
 add();
 subtract();
